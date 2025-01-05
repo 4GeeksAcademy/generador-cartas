13c733f25599e2ed2d2c70dc0e41a39a50d61022
kazou92 <99810202+kazou92@users.noreply.github.com>
2025-01-05 01:58:05 +0000

    proyecto generador de cartas subido

--- src/app.js
+++ src/app.js
@@ -7,5 +7,61 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  // console.log("Hello Rigo from the console!");
+  // alert("HOLA");
+
+  let cardIcons = [
+    '<i class="bi bi-suit-heart-fill"></i>', // Corazón
+    '<i class="bi bi-suit-diamond-fill"></i>', // Diamante
+    '<i class="bi bi-suit-club-fill"></i>', // Trébol
+    '<i class="bi bi-suit-spade-fill"></i>' // Pica
+  ];
+  let cartas = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "AS"
+  ];
+
+  let indexRandom = arr => {
+    return Math.floor(Math.random() * arr.length);
+  };
+
+  let btn = document.querySelector(".button");
+  console.log(btn);
+
+  let num = document.querySelector(".number");
+  let icon = document.querySelectorAll(".icon");
+
+  btn.addEventListener("click", () => {
+    let indexCartas = indexRandom(cartas);
+    let indexIcons = indexRandom(cardIcons);
+    num.innerHTML = cartas[indexCartas];
+    icon.forEach(x => {
+      x.innerHTML = cardIcons[indexIcons];
+    });
+  });
+  let boolean = true;
+
+  setInterval(() => {
+    if (boolean) {
+      let indexCartas = indexRandom(cartas);
+      let indexIcons = indexRandom(cardIcons);
+      num.innerHTML = cartas[indexCartas];
+      icon.forEach(x => {
+        x.innerHTML = cardIcons[indexIcons];
+      });
+    } else {
+      return !boolean;
+    }
+  }, 1000);
 };
